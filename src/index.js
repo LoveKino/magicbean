f5149910ccac3b7de05ea94eda78b36f05ea41b4
loveKino <842914439@qq.com>
2016-06-15 18:38:29 +0800

    init

---
+++ src/index.js
@@ -0,0 +1,11 @@
+/**
+ * data flow
+ *
+ * 1. composable
+ *
+ * a big data can be split into parts
+ *
+ * one task applied to data, can be split into some smaller tasks.
+ *
+ * 2. declare concerned data
+ */
